655074055adbd659cae7ca7a633e212b34884e6c
Sean D Kim <sean.kim@merge.dev>
2025-11-01 10:01:59 -0700

    delete everything

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { useAgentHandlerChat } from './useAgentHandlerChat';
-export type {
-  UseAgentHandlerChatProps,
-  UseAgentHandlerChatResponse,
-  TenantConfig,
-  Environment,
-} from './types';
